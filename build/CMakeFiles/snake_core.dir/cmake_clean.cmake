f82c316475ba221cbfb7bcc96cc6f3cb941ec076
GVA <georgios.vassos1@maersk.com>
2026-04-05 16:43:05 +0300

    chore: restructure project layout with CMake and src/include/tests dirs

---
+++ build/CMakeFiles/snake_core.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake_core.dir/src/List.cpp.o"
+  "CMakeFiles/snake_core.dir/src/List.cpp.o.d"
+  "CMakeFiles/snake_core.dir/src/Point.cpp.o"
+  "CMakeFiles/snake_core.dir/src/Point.cpp.o.d"
+  "CMakeFiles/snake_core.dir/src/ShitList.cpp.o"
+  "CMakeFiles/snake_core.dir/src/ShitList.cpp.o.d"
+  "CMakeFiles/snake_core.dir/src/Terrain.cpp.o"
+  "CMakeFiles/snake_core.dir/src/Terrain.cpp.o.d"
+  "CMakeFiles/snake_core.dir/src/mygetch.cpp.o"
+  "CMakeFiles/snake_core.dir/src/mygetch.cpp.o.d"
+  "libsnake_core.a"
+  "libsnake_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snake_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
